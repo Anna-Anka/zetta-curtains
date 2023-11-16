bbf46d877ecbc07a120d7a545a2ac14ca2d33daa
Anna-Anka <anjbekrenewa@gmail.com>
2023-11-16 14:09:42 +0300

    finish adaptive for header

---
+++ src/js/_include.js
@@ -0,0 +1,4 @@
+import { burger } from './templates/_burger';
+import './utils/_header-height';
+
+burger();
--- src/js/utils/_header-height.js
+++ src/js/utils/_header-height.js
@@ -1,6 +1,14 @@
-export const getHeaderHeight = () => {
+const getHeaderHeight = () => {
     if (document.querySelector('.header')) {
         const headerHeight = document.querySelector('.header').offsetHeight;
         document.querySelector(':root').style.setProperty('--header-height', `${headerHeight}px`);
     }
 };
+
+getHeaderHeight();
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth < 992) {
+        getHeaderHeight();
+    }
+});
