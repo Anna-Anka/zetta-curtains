796b45807056a2e15c4290ccecc83939eef5bc7f
Anna-Anka <anjbekrenewa@gmail.com>
2023-11-19 12:21:28 +0300

    add portfolio

---
+++ src/js/_project.js
@@ -0,0 +1,1 @@
+import './project/swipers/_portfolio-swiper';
---
+++ src/js/project/swipers/_portfolio-swiper.js
@@ -0,0 +1,51 @@
+import Swiper, {
+    Navigation, Scrollbar, Autoplay,
+} from 'swiper';
+
+Swiper.use([Navigation, Scrollbar, Autoplay]);
+
+if (document.querySelector('.portfolio__swiper')) {
+    new Swiper('.portfolio__swiper', {
+        speed: 800,
+
+        // autoplay: {
+        //     delay: 2000,
+        //     disableOnInteraction: false,
+        //     stopOnlastSlide: false,
+        // },
+
+        navigation: {
+            nextEl: '.portfolio__button--next',
+            prevEl: '.portfolio__button--prev',
+        },
+
+        scrollbar: {
+            el: '.portfolio__scrollbar',
+            draggable: true,
+        },
+
+        breakpoints: {
+            1200: {
+                slidesPerGroup: 4,
+                slidesPerView: 4,
+            },
+
+            992: {
+                slidesPerGroup: 3,
+                slidesPerView: 3,
+            },
+
+            560: {
+                spaceBetween: 2,
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+
+            0: {
+                spaceBetween: 1,
+                slidesPerView: 1,
+                spaceBetween: 10,
+            },
+        },
+    });
+}
